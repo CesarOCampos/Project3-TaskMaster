feb4201c227d29b9f40d95fc46d15cd551131cd0
George Huliaris <georgehuliaris@me.com>
2021-05-12 13:40:18 -0500

    adding files

---
+++ client/src/components/Contacts.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import {
+  TextField,
+  Typography,
+  // Button,
+  Grid,
+  Box,
+  // Avatar,
+} from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+import Navbar from "./Navbar";
+// import avatar from "../luckyme.png";
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    position: "absolute",
+    opacity: ".95",
+  },
+  button: {
+    marginTop: "1rem",
+    color: "tomato",
+    borderColor: "tomato",
+  },
+  // avatar: {
+  //   width: theme.spacing(15),
+  //   height: theme.spacing(15),
+  //   margin: theme.spacing(9),
+  // },
+}));
+
+const InputField = withStyles({
+  root: {
+    "& label.Mui-focused": {
+      color: "tomato",
+    },
+    "& label": {
+      color: "tan",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "tan",
+      },
+      "&:hover fieldset": {
+        borderColor: "tomato",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "tan",
+      },
+    },
+  },
+})(TextField);
+
+const Contacts = () => {
+  const classes = useStyles();
+  return (
+    <Box
+      component="div"
+      style={{ background: "#233", height: "150vh", opacity: ".95" }}
+    >
+      <Navbar />
+
+      <Grid container justify="center">
+        <Box component="form" className={classes.form}>
+          {/* <Avatar
+            className={classes.avatar}
+            src={avatar}
+            alt="George Huliaris"
+          /> */}
+          <Typography
+            variant="h5"
+            style={{
+              color: "tomato",
+              textAlign: "center",
+              textTransform: "uppercase",
+            }}
+          >
+            Hire or contact me!
+          </Typography>
+        </Box>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Contacts;
---
+++ client/src/components/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Header from "./Header";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  particlesCanva: {
+    position: "absolute",
+    opacity: "0.3",
+  },
+});
+
+const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
+};
+
+export default Home;
