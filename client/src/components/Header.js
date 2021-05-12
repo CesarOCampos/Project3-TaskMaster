2f8914370570cd0465293923d2888fe4e837bfda
George Huliaris <georgehuliaris@me.com>
2021-05-12 15:50:32 -0500

    adding component files

---
+++ client/src/components/Header.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Avatar, Grid, Box } from "@material-ui/core";
+// import avatar from "";
+import Typed from "react-typed";
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    margin: theme.spacing(1),
+  },
+  title: {
+    color: "tomato",
+  },
+  subtitle: {
+    color: "tan",
+    marginBottom: "3rem",
+  },
+  typedContainer: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "100vw",
+    textAlign: "center",
+    zIndex: 1,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <div style={{ background: "#233", height: "150vh", opacity: ".95" }}>
+      <Box className={classes.typedContainer}>
+        <Grid container justify="center">
+          {/* <Avatar className={classes.avatar} src={avatar} alt="" /> */}
+        </Grid>
+        <Typography className={classes.title} variant="h4">
+          <Typed
+            strings={["Task Master"]}
+            typeSpeed={210}
+            onComplete={(self) => self.cursor.remove()}
+          />
+        </Typography>
+        <br />
+        <Typography className={classes.subtitle} variant="h5">
+          <Typed
+            strings={[
+              "We can write whatever we want here if we decide to keep it",
+            ]}
+            startDelay={4700}
+            typeSpeed={50}
+            // backSpeed={30}
+            // backDelay={1200}
+            // loop
+          />
+        </Typography>
+
+        <Grid container justify="center">
+          <Typography className={classes.title} variant="h5">
+            Make sure you never miss a deadline!
+          </Typography>
+        </Grid>
+      </Box>
+    </div>
+  );
+};
+
+export default Header;
---
+++ client/src/components/Portfolio.js
@@ -0,0 +1,232 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  // Avatar,
+  Box,
+  Grid,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import Navbar from "./Navbar";
+// import project1 from "../images/portfolio.png";
+// import project2 from "../images/password_gen.png";
+// import project3 from "../images/cryptostock.png";
+// import project4 from "../images/ExpressNext.png";
+// import avatar from "../thankfulme.png";
+import LaunchIcon from "@material-ui/icons/Launch";
+import GitHubIcon from "@material-ui/icons/GitHub";
+
+const useStyles = makeStyles((theme) => ({
+  mainContainer: {
+    background: "#233",
+    height: "500vh",
+    opacity: ".96",
+  },
+  cardContainer: {
+    maxWidth: 345,
+    margin: "5rem auto",
+  },
+  avatar: {
+    display: "block",
+    margin: "0.5rem auto",
+    width: theme.spacing(12),
+    height: theme.spacing(12),
+  },
+}));
+
+const Portfolio = () => {
+  const classes = useStyles();
+  return (
+    <Box component="div" className={classes.mainContainer}>
+      <Navbar />
+
+      <Grid container justify="center">
+        {/* Project 1 */}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardMedia
+              component="img"
+              alt="Task 1"
+              height="140"
+              // image={project1}
+            />
+
+            <CardContent>
+              <Typography gutterBottom variant="h5">
+                Task 1
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Notes
+              </Typography>
+            </CardContent>
+
+            <CardActions>
+              <Button
+                href=""
+                target="_blank"
+                size="small"
+                color="tomato"
+                variant="contained"
+                startIcon={<GitHubIcon />}
+              >
+                GitHub
+              </Button>
+
+              <Button
+                size="small"
+                color="tomato"
+                href=""
+                target="_blank"
+                variant="contained"
+                startIcon={<LaunchIcon />}
+              >
+                Visit Page
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        {/* Project 2 */}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardMedia
+              component="img"
+              alt="Task 2"
+              height="140"
+              // image={project2}
+            />
+
+            <CardContent>
+              <Typography gutterBottom variant="h5">
+                Task 2
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Notes
+              </Typography>
+            </CardContent>
+
+            <CardActions>
+              <Button
+                href=""
+                target="_blank"
+                size="small"
+                color="tomato"
+                variant="contained"
+                startIcon={<GitHubIcon />}
+              >
+                GitHub
+              </Button>
+
+              <Button
+                size="small"
+                color="tomato"
+                href=""
+                target="_blank"
+                variant="contained"
+                startIcon={<LaunchIcon />}
+              >
+                Visit Page
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        {/* Project 3 */}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardMedia
+              component="img"
+              alt="Task 3"
+              height="140"
+              // image={project3}
+            />
+
+            <CardContent>
+              <Typography gutterBottom variant="h5">
+                Task 3
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Notes
+              </Typography>
+            </CardContent>
+
+            <CardActions>
+              <Button
+                href=""
+                size="small"
+                color="tomato"
+                target="_blank"
+                variant="contained"
+                startIcon={<GitHubIcon />}
+              >
+                GitHub
+              </Button>
+
+              <Button
+                size="small"
+                color="tomato"
+                href=""
+                target="_blank"
+                variant="contained"
+                startIcon={<LaunchIcon />}
+              >
+                Visit Page
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        {/* Project 4 */}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardMedia
+              component="img"
+              alt="Task 4"
+              height="140"
+              // image={project4}
+            />
+
+            <CardContent>
+              <Typography gutterBottom variant="h5">
+                Task 4
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Notes
+              </Typography>
+            </CardContent>
+
+            <CardActions>
+              <Button
+                href=""
+                target="_blank"
+                size="small"
+                color="tomato"
+                variant="contained"
+                startIcon={<GitHubIcon />}
+              >
+                GitHub
+              </Button>
+
+              <Button
+                size="small"
+                color="tomato"
+                href=""
+                target="_blank"
+                variant="contained"
+                startIcon={<LaunchIcon />}
+              >
+                Visit Page
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+      {/* <Avatar className={classes.avatar} src={avatar} alt="George Huliaris" /> */}
+    </Box>
+  );
+};
+
+export default Portfolio;
