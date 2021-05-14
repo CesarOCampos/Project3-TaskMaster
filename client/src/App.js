45b79e4beb80e37b2f22c37c45766847bc52dc72
Susan Fujii <susan.fujii@me.com>
2021-05-13 20:44:19 -0500

    Created /profile route and displayed user token info we receive from Auth0. No Profile link in navbar created yet.

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
 import Home from "./components/";
 import Projects from "./pages/projects/Projects";
+import Profile from "./pages/profile/Profile";
 import Portfolio from "./components/Portfolio";
 import Contacts from "./components/Contacts";
 import Loading from "./components/loading";
@@ -21,6 +22,9 @@ function App() {
     <>
     <CssBaseline />
     <Route exact path="/" component={Home}/>
+
+    <Route path="/profile" component={Profile} />
+
     <Route path="/projects" component={Projects}/>
     <Route path="/portfolio" component={Portfolio}/>
     <Route path="/contacts" component={Contacts}/>
