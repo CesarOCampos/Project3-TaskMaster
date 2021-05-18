8dfaa843f9a734acd9b1060f0dc82fe0551819ce
Susan <79765040+srfujii@users.noreply.github.com>
2021-05-18 12:17:45 -0500

    Fixed Profile page display issue.

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -11,18 +11,19 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
-
+import Avatar from "@material-ui/core/Avatar";
 import "./style.css";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
   },
   media: {
-    height: 140,
+    height: 200,
   },
   mainContainer: {
     background: "#A3BCB6",
-    height: "100vh",
+    height: "120vh",
     opacity: ".95",
   },
   heading: {
@@ -34,12 +35,6 @@ const useStyles = makeStyles((theme) => ({
     color: "white",
     padding: "0",
     textTransform: "uppercase",
-  },
-  avatar: {
-    display: "block",
-    margin: "0.5rem auto",
-    width: theme.spacing(15),
-    height: theme.spacing(15),
   },
 }));
 
@@ -58,23 +53,28 @@ const Profile = () => {
           User Profile
           <br />
           <br />
-          <Card className={classes.root} className="card">
+          <Card className={classes.root} className="card profile-card">
 
             <CardActionArea>
               <CardContent>
-
-                <Typography gutterBottom variant="h5" component="h2">
-                  {name}
-                  {email}
-                  
-                </Typography>
-
+                <Avatar alt={name} src={picture} className={classes.large}/>
                 <Typography variant="body2" color="textSecondary" component="p">
                   <img
                     src={picture}
                     alt="Profile"
                   />
                 </Typography>
+
+                <Typography gutterBottom variant="h5" component="h2" >
+                  {name}
+                </Typography>
+                
+                <Typography gutterBottom variant="h5" component="h2">
+                  {email}
+                </Typography>
+
+                
+
 
                 <Typography variant="body2" color="textSecondary" component="p">
                   {user.email}
